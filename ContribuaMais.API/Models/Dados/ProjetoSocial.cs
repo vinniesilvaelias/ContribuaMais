083104a6283fa8c6013af96cea2a58bf90b265da
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-18 16:41:15 -0300

    Criação da entidade projeto social.

---
+++ ContribuaMais.API/Models/Dados/ProjetoSocial.cs
@@ -0,0 +1,10 @@
+﻿using ContribuaMais.API.Models.TiposBase;
+
+namespace ContribuaMais.API.Models.Dados
+{
+    public class ProjetoSocial : EntidadeBase
+    {
+        public string Nome { get; set; }
+        public string Cnpj { get; set; }
+    }
+}
