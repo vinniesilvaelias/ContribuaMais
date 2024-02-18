eb10ce1f67430ea4027c8f7ded352c81ba13230d
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-18 16:51:38 -0300

    Criação do Dto para o projeto social.

---
+++ ContribuaMais.API/Models/Dtos/DtoProjetoSocial.cs
@@ -0,0 +1,8 @@
+﻿namespace ContribuaMais.API.Models.Dtos
+{
+    public class DtoProjetoSocial : DtoBase
+    {
+        public string Nome { get; set; }
+        public string Cnpj { get; set; }
+    }
+}
