11c7585f1447849ff1cc96b87a9ab5e93e6c6605
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-18 17:36:15 -0300

    Adição das propriedades para abstrair os valores arrecadados e o objetivo da campanha.

--- ContribuaMais.API/Models/Dtos/DtoCampanha.cs
+++ ContribuaMais.API/Models/Dtos/DtoCampanha.cs
@@ -6,5 +6,7 @@
         public DateTime Inicio { get; set; }
         public DateTime Fim { get; set; }
         public bool EstaAtiva { get; set; }
+        public decimal Objetivo { get; set; }
+        public decimal Arrecadado { get; set; }
     }
 }
