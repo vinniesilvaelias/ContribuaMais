d2decd673c52044e743a9c021327abeb2170045b
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-18 16:48:44 -0300

    Criação do Dto item.

---
+++ ContribuaMais.API/Models/Dtos/DtoItem.cs
@@ -0,0 +1,10 @@
+﻿using ContribuaMais.API.Models.Enumeradores;
+
+namespace ContribuaMais.API.Models.Dtos
+{
+    public class DtoItem : DtoBase
+    {
+        public string Decricao { get; set; }
+        public EnumTipoItem Tipo { get; set; }
+    }
+}
