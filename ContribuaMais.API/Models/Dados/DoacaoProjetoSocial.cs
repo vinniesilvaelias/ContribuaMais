4dda05ea685034fbcee7e76eae9294c7aaa56bb3
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-18 16:44:33 -0300

    Criação da entidade doação/projeto social.

---
+++ ContribuaMais.API/Models/Dados/DoacaoProjetoSocial.cs
@@ -0,0 +1,8 @@
+﻿namespace ContribuaMais.API.Models.Dados
+{
+    public class DoacaoProjetoSocial
+    {
+        public Guid IdDoaco { get; set; }
+        public Guid IdProjetoSocial { get; set; }
+    }
+}
