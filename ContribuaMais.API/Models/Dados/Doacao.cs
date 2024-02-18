041043f2f25a09bece7ba3968fae591182ed06d8
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-18 16:43:26 -0300

    Criação da entidade doação.

---
+++ ContribuaMais.API/Models/Dados/Doacao.cs
@@ -0,0 +1,11 @@
+﻿using ContribuaMais.API.Models.TiposBase;
+
+namespace ContribuaMais.API.Models.Dados
+{
+    public class Doacao : EntidadeBase
+    {
+        public Guid IdItem { get; set; }
+        public Guid IdDoador { get; set; }
+        public Guid IdCampanha { get; set; }
+    }
+}
