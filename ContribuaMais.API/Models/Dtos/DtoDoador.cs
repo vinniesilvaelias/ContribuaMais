cd1b1c768275870d1a71b4bb7764949a06829b1c
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-18 16:50:31 -0300

    Criação do Dto Doador.

---
+++ ContribuaMais.API/Models/Dtos/DtoDoador.cs
@@ -0,0 +1,9 @@
+﻿namespace ContribuaMais.API.Models.Dtos
+{
+    public class DtoDoador : DtoBase
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public string Endereco { get; set; }
+    }
+}
