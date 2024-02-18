83ba1d416ee76cf75b9c07dc36f059a73ac67f57
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-18 18:40:45 -0300

    Implementação do controlador de doador.

--- ContribuaMais.API/Controllers/DoadorController.cs
+++ ContribuaMais.API/Controllers/DoadorController.cs
@@ -1,46 +1,84 @@
-﻿//using ContribuaMais.API.Models.Dados;
-//using Microsoft.AspNetCore.Mvc;
+﻿using ContribuaMais.API.Models.Dados;
+using ContribuaMais.API.Models.Dtos;
+using ContribuaMais.API.Models.TiposBase;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace ContribuaMais.API.Controllers
-//{
-//    [ApiController]
-//    [Route("[controller]")]
-//    public class DoadorController : ControladorBase<Doador>
-//    {
-//        protected override void Imprima(Doador EntidadeBase)
-//        {
-//            Console.WriteLine(EntidadeBase.Codigo);
-//            Console.WriteLine(EntidadeBase.Nome);
-//            Console.WriteLine(EntidadeBase.Cpf);
-//            Console.WriteLine(EntidadeBase.Endereco);
-//        }
+namespace ContribuaMais.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DoadorController : ControladorBase<DtoDoador, Doador>
+    {
+        protected override void Imprima(DtoDoador dto)
+        {
+            Console.Clear();
+            Console.WriteLine(dto.Cpf);
+            Console.WriteLine(dto.Endereco);
+        }
 
-//        protected override void MockItens()
-//        {
-//            Itens = new List<Doador> 
-//            {
-//                new Doador
-//                {
-//                    Codigo = 1,
-//                    Nome = "Joao",
-//                    Endereco = "Rua A",
-//                    Cpf = "111111"
-//                },
-//                new Doador
-//                {
-//                    Codigo = 2,
-//                    Nome = "Maria",
-//                    Endereco = "Rua B",
-//                    Cpf = "222222"
-//                },
-//                new Doador
-//                {
-//                    Codigo = 3,
-//                    Nome = "Jose",
-//                    Endereco = "Rua C",
-//                    Cpf = "333333"
-//                },
-//            };
-//        }
-//    }
-//}
+        protected override Doador Converta(DtoDoador dto)
+        {
+            var objeto = Entidades.Where(x => x.Codigo == dto.Codigo).FirstOrDefault();
+
+            if (objeto == null)
+            {
+                objeto = new Doador
+                {
+                    Codigo = Entidades.Count + 1,
+                    Nome = dto.Nome,
+                    Cpf = dto.Cpf,
+                    Id = Guid.NewGuid()
+                };
+            }
+
+            return objeto;
+        }
+
+        protected override DtoDoador Converta(Doador objeto)
+        {
+            var dto = Dtos.Where(x => x.Codigo == objeto.Codigo).FirstOrDefault();
+
+            if (dto == null)
+            {
+                dto = new DtoDoador
+                {
+                    Codigo = Dtos.Count + 1,
+                    Nome = dto.Nome,
+                    Cpf = dto.Cpf,
+                };
+            }
+
+            return dto;
+        }
+
+        protected override void MockEntidades()
+        {
+            Entidades = Dtos.Select(Converta).ToList();
+        }
+
+        protected override void MockDtos()
+        {
+            Dtos = new List<DtoDoador>
+            {
+                new DtoDoador
+                {
+                    Codigo = 1,
+                    Nome = "João",
+                    Cpf = "1111111111"
+                },
+                new DtoDoador
+                {
+                    Codigo = 2,
+                    Nome = "Maria",
+                    Cpf = "22222222"
+                },
+                new DtoDoador
+                {
+                    Codigo = 3,
+                    Nome = "José",
+                    Cpf = "333333"
+                },
+            };
+        }
+    }
+}
--- ContribuaMais.API/Controllers/ProjetoSocialController.cs
+++ ContribuaMais.API/Controllers/ProjetoSocialController.cs
@@ -1,38 +1,38 @@
-﻿//using ContribuaMais.API.Models.Dados;
-//using Microsoft.AspNetCore.Mvc;
+﻿using ContribuaMais.API.Models.Dados;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace ContribuaMais.API.Controllers
-//{
-//    [ApiController]
-//    [Route("[controller]")]
-//    public class ProjetoSocialController : ControladorBase<ProjetoSocial>
-//    {
-//        protected override void Imprima(ProjetoSocial EntidadeBase)
-//        {
-//            Console.WriteLine(EntidadeBase.Nome);
-//            Console.WriteLine(EntidadeBase.Cnpj);
-//        }
+namespace ContribuaMais.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProjetoSocialController : ControladorBase<ProjetoSocial>
+    {
+        protected override void Imprima(ProjetoSocial EntidadeBase)
+        {
+            Console.WriteLine(EntidadeBase.Nome);
+            Console.WriteLine(EntidadeBase.Cnpj);
+        }
 
-//        protected override void MockItens()
-//        {
-//            Itens = new List<ProjetoSocial>
-//            {
-//                new ProjetoSocial
-//                {
-//                    Nome = "OVG",
-//                    Cnpj = "11111111"
-//                },
-//                new ProjetoSocial
-//                {
-//                    Nome = "Teletom",
-//                    Cnpj = "222222"
-//                },
-//                new ProjetoSocial
-//                {
-//                    Nome = "Criança Esperança",
-//                    Cnpj = "3333333"
-//                },
-//            };
-//        }
-//    }
-//}
+        protected override void MockEntidades()
+        {
+            Entidades = new List<ProjetoSocial>
+            {
+                new ProjetoSocial
+                {
+                    Nome = "OVG",
+                    Cnpj = "11111111"
+                },
+                new ProjetoSocial
+                {
+                    Nome = "Teletom",
+                    Cnpj = "222222"
+                },
+                new ProjetoSocial
+                {
+                    Nome = "Criança Esperança",
+                    Cnpj = "3333333"
+                },
+            };
+        }
+    }
+}
