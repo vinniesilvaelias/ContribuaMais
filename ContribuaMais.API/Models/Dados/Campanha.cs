f29bb7a1a22671289778c613aa0afd1024623ad9
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-18 16:37:31 -0300

    Criação da entidade Campanha.

---
+++ ContribuaMais.API/Models/Dados/Campanha.cs
@@ -0,0 +1,12 @@
+﻿using ContribuaMais.API.Models.TiposBase;
+
+namespace ContribuaMais.API.Models.Dados
+{
+    public class Campanha : EntidadeBase
+    {
+        public string Descricao { get; set; }
+        public DateTime Inicio { get; set; }
+        public DateTime Fim { get; set; }
+        public bool EstaAtiva { get; set; }
+    }
+}
