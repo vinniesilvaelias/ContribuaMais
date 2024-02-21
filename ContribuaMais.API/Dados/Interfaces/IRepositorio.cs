390940449dc29ff1d78d3d24f9c85fa7de66f9c4
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-21 19:46:13 -0300

    Implementação do repositório genérico.

---
+++ ContribuaMais.API/Dados/Interfaces/IRepositorio.cs
@@ -0,0 +1,14 @@
+﻿using ContribuaMais.API.Models.TiposBase;
+
+namespace ContribuaMais.API.Dados.Interfaces
+{
+    public interface IRepositorio<TEntidade>
+        where TEntidade : EntidadeBase
+    {
+        void Atualize(TEntidade entidade);
+        void Cadastre(TEntidade entidade);
+        TEntidade Consulte(Guid id);
+        TEntidade Consulte(int codigo);
+        TEntidade Exclua(Guid id);
+    }
+}
---
+++ ContribuaMais.API/Dados/Repositorios/RepositorioGenerico.cs
@@ -0,0 +1,50 @@
+﻿using ContribuaMais.API.Dados.Interfaces;
+using ContribuaMais.API.Models.TiposBase;
+
+namespace ContribuaMais.API.Dados.Repositorios
+{
+    public class RepositorioGenerico<TEntidade> : IRepositorio<TEntidade>
+        where TEntidade : EntidadeBase
+    {
+        private ContribuaMaisContexto _contexto;
+
+        public RepositorioGenerico(ContribuaMaisContexto contexto)
+        {
+            _contexto  = contexto;
+        }
+        public void Atualize(TEntidade entidade)
+        {
+            _contexto.Update(entidade);
+            _contexto.SaveChanges();
+        }
+
+        public void Cadastre(TEntidade entidade)
+        {
+            _contexto.Add(entidade);
+        }
+
+        public TEntidade Consulte(Guid id)
+        {
+            var entidade = _contexto.Find<TEntidade>(id);
+
+            return entidade;
+        }
+
+        public TEntidade Consulte(int codigo)
+        {
+
+            var entidade = _contexto.Find<TEntidade>(codigo);
+
+            return entidade;
+        }
+
+        public TEntidade Exclua(Guid id)
+        {
+            var entidade = _contexto.Find<TEntidade>(id);
+
+            _contexto.Remove(entidade);
+
+            return entidade;
+        }
+    }
+}
