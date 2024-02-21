5d79c93577e018254e722ef751ddbaa090c0eff4
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-02-21 19:45:48 -0300

    Criação do banco de dados.

---
+++ ContribuaMais.API/Dados/ContribuaMaisContexto.cs
@@ -0,0 +1,21 @@
+﻿using ContribuaMais.API.Dados.Interfaces;
+using ContribuaMais.API.Models.Dados;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContribuaMais.API.Dados
+{
+    public class ContribuaMaisContexto : DbContext, IContribuaMaisRepositorio
+    {
+        public ContribuaMaisContexto(DbContextOptions<ContribuaMaisContexto> opcoes)
+            : base(opcoes)
+        {
+        }
+
+        public DbSet<Campanha> Campanhas { get; set; }
+        public DbSet<Doacao> Doacoes { get; set; }
+        public DbSet<Doador> Doadores { get; set; }
+        public DbSet<Item> Itens { get; set; }
+        public DbSet<ProjetoSocial> ProjetoSociais { get; set; }
+
+    }
+}
---
+++ ContribuaMais.API/Migrations/20240220193343_CriacaoBaseDeDados.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ContribuaMais.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoBaseDeDados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Campanhas",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Inicio = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Fim = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EstaAtiva = table.Column<bool>(type: "bit", nullable: false),
+                    Objetivo = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false),
+                    Arrecadado = table.Column<decimal>(type: "decimal(2,2)", precision: 2, nullable: false),
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Campanhas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doadores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cpf = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Endereco = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doadores", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Itens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Decricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tipo = table.Column<int>(type: "int", nullable: false),
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Itens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjetoSocials",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cnpj = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjetoSocials", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Doacoes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ItemId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DoadorId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CampanhaId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FoiEfetivada = table.Column<bool>(type: "bit", nullable: false),
+                    Quantidade = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Codigo = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Doacoes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Doacoes_Campanhas_CampanhaId",
+                        column: x => x.CampanhaId,
+                        principalTable: "Campanhas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Doacoes_Doadores_DoadorId",
+                        column: x => x.DoadorId,
+                        principalTable: "Doadores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Doacoes_Itens_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Itens",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doacoes_CampanhaId",
+                table: "Doacoes",
+                column: "CampanhaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doacoes_DoadorId",
+                table: "Doacoes",
+                column: "DoadorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Doacoes_ItemId",
+                table: "Doacoes",
+                column: "ItemId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Doacoes");
+
+            migrationBuilder.DropTable(
+                name: "ProjetoSocials");
+
+            migrationBuilder.DropTable(
+                name: "Campanhas");
+
+            migrationBuilder.DropTable(
+                name: "Doadores");
+
+            migrationBuilder.DropTable(
+                name: "Itens");
+        }
+    }
+}
