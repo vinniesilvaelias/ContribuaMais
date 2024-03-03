daff01752543648d66d714416dfd72f8a48801d3
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-03-03 11:12:52 -0300

    Criação dos repositorios de conceitos de infraestrutura.

---
+++ ContribuaMais.API/Dados/Repositorios/Infraestrutura/RepositorioDocumentosPessoais.cs
@@ -0,0 +1,11 @@
+﻿using ContribuaMais.API.Models.Dados.Infraestrutura;
+
+namespace ContribuaMais.API.Dados.Repositorios.Infraestrutura
+{
+    public class RepositorioDocumentosPessoais : RepositorioBase<DocumentosPessoais>
+    {
+        public RepositorioDocumentosPessoais(ContribuaMaisContexto contexto) : base(contexto)
+        {
+        }
+    }
+}
---
+++ ContribuaMais.API/Dados/Repositorios/Infraestrutura/RepositorioPessoa.cs
@@ -0,0 +1,11 @@
+﻿using ContribuaMais.API.Models.Dados.Infraestrutura;
+
+namespace ContribuaMais.API.Dados.Repositorios.Infraestrutura
+{
+    public class RepositorioPessoa : RepositorioBase<Pessoa>
+    {
+        public RepositorioPessoa(ContribuaMaisContexto contexto) : base(contexto)
+        {
+        }
+    }
+}
