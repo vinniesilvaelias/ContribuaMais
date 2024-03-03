62114fdf2b29d961e6e570731be04a6d86e50dd7
VINICIUS ELIAS DA SILVA <20191002800895@pucgo.edu.br>
2024-03-03 11:07:18 -0300

    Criação dos conversores para os conceitos de infraestrutura.

---
+++ ContribuaMais.API/Profiles/Infraestrutura/DocumentosPessoaisProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ContribuaMais.API.Models.Dados.Infraestrutura;
+using ContribuaMais.API.Models.Dtos.Infraestrutura;
+
+namespace ContribuaMais.API.Profiles.Infraestrutura
+{
+    public class DocumentosPessoaisProfile : Profile
+    {
+        public DocumentosPessoaisProfile()
+        {
+            CreateMap<DocumentosPessoais, DtoDocumentosPessoais>();
+            CreateMap<DtoDocumentosPessoais, DocumentosPessoais>();
+        }
+    }
+}
